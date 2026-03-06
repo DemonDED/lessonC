a02d67e1f4ad7d371f7d65dd6739f720c4879577
DemonDED <DemonDED@inbox.ru>
2026-03-06 16:33:12 +0700

    task on lesson5

--- lesson5/tasks/task2/main.c
+++ lesson5/tasks/task2/main.c
@@ -20,6 +20,7 @@ int main() {
 int enter_number_for_array(int array_int[], int entered_number) {
 	if (scanf("%d", &entered_number) == 1) {
 		// Array operations
+		
 		for (int i = 0; i <= SIZE_ARR1; i++) {
 			if (array_int[i] != entered_number) {
 				array_int[i] = entered_number;
