dd94f9fe87c96a0416ae804638a6162d97154fd7
DemonDED <DemonDED@inbox.ru>
2026-03-16 17:10:00 +0700

    bubble ascending and descending

--- lesson6/example_function_pointer.c
+++ lesson6/example_function_pointer.c
@@ -35,3 +35,34 @@ int main() {
 
 	return 0;
 }
+
+void bubble(int work[], const int size, in (*compare) (int a, int b)) {
+	int pass;
+	int count;
+
+	void swap(int *element1Ptr, int *element2Ptr);
+
+	for (pass = 1; pass < size - 1; pass++) {
+		for (count = 0; count < size; count++) {
+			if ( (*compare) (work[count], work[count + 1]) ) {
+				swap(&work[count], &work[count + 1]);
+			}
+		}
+	}
+}
+
+void swap(int *element1Ptr, int *element2Ptr) {
+	int hold;
+
+	hold = *element1Ptr;
+	*element1Ptr = *element2Ptr;
+	*element2Ptr = hold;
+}
+
+int ascending(int a, int b) {
+	return b < a;
+}
+
+int descending(int a, int b) {
+	return b > a;
+}
